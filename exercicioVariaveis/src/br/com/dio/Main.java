473f334a084d26fdbe817b1ceaa16cb2d3f358e0
Paulo Sergio <sergiopaulosilvajr@gmail.com>
2021-11-10 19:02:20 -0300

    arquivos para exercicios de variaveis e operadores relacionais

---
+++ exercicioVariaveis/src/br/com/dio/Main.java
@@ -0,0 +1,30 @@
+package br.com.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        byte b1 = 10;
+        byte b2 = 20;
+
+        short s1 = 20000;
+        short s2 = 30000;
+
+        int i1 = -10000000;
+        int i2 = 28500;
+
+        long l1 = 10000000000L;
+        long l2 = 30000000000L;
+
+        float f1 = 4.5F;
+
+        double d1 = 85.69;
+
+        char c1 = 't';
+
+        String st1 = "tested";
+
+        System.out.println("Valor de B1 byte " + b1);
+        
+    }
+}
---
+++ operadoresRelacionais/src/br/com/dio/Main.java
@@ -0,0 +1,19 @@
+package br.com.dio;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int i1 = 10;
+        int i2 = 20;
+
+        float f1 = 4.5F;
+        float f2 = 3.5F;
+
+        char c1 = 'x';
+        char c2 = 'x';
+
+        
+
+    }
+}
